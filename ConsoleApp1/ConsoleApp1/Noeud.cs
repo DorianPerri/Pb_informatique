b840bc04fada3315671461b6d8ce1ec4c3aa633a
DorianPerri <dorian.perri@edu.devinci.fr>
2025-04-04 15:36:48 +0200

    Update Noeud.cs

--- ConsoleApp1/ConsoleApp1/Noeud.cs
+++ ConsoleApp1/ConsoleApp1/Noeud.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +8,60 @@ namespace ConsoleApp1
 {
     internal class Noeud
     {
-        public int id;
-        public Noeud(int ide) { 
-            this.id = ide;
-        } 
-        public int Id
+        //attributs
+        public int id_station;
+        public string libelle_ligne;
+        public string libelle_station;
+        public float longitude;
+        public float latitude;
+        public string commune;
+        public int code_commune;
+
+        //constructeur
+        public Noeud(int id_station, string libelle_ligne, string libelle_station, float longitude, float latitude,
+            string commune, int code_commune)
         {
-            get { return this.id; }
-            set { this.id = value; }
+            this.id_station = id_station;
+            this.libelle_ligne = libelle_ligne;
+            this.libelle_station = libelle_station;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.commune = commune;
+            this.code_commune = code_commune;
+        }
+
+        //get/set
+        public int Id_station
+        {
+            get { return this.id_station; }
+        }
+        public string Libelle_ligne
+        {
+            get { return this.libelle_ligne; }
+            set { this.Libelle_ligne = value; }
+        }
+        public string Libelle_station
+        {
+            get { return this.libelle_station; }
+            set { this.libelle_station = value; }
+        }
+        public float Latitude
+        {
+            get { return this.latitude; }
+        }
+        public float Longitude
+        {
+            get { return this.longitude; }
+        }
+        public string Commune
+        {
+            get { return this.commune; }
+        }
+        public int Code_commune
+        {
+            get { return this.code_commune; }
         }
     }
+
+
 }
